32e41c8cd56af2d7ecea98723bf27379be7b98e7
Manuel Vilche <manuelvilche@gmail.com>
2020-09-18 19:11:10 -0300

    update dependencies and add github actions

--- tests/client-settings-model.js
+++ tests/client-settings-model.js
@@ -8,10 +8,6 @@ describe('Original Order Model', () => {
 
 	describe('Getters', () => {
 
-		it('Should not return any databaseKey', () => {
-			assert.deepStrictEqual(Model.prototype.databaseKey, undefined);
-		});
-
 		it('Should return the table name', () => {
 			assert.deepStrictEqual(Model.table, 'settings');
 		});
